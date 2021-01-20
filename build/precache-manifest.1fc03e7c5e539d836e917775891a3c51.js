64b1f7ebb5b0609740c31e0801604d794c3f2163
Sanjay Murmu <sanjaymurmu40work@gmail.com>
2021-01-21 02:28:28 +0530

    project Front End 2021

---
+++ build/precache-manifest.1fc03e7c5e539d836e917775891a3c51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f770bf730838c6ee5a9",
+    "url": "/static/css/main.52718c89.chunk.css"
+  },
+  {
+    "revision": "4f770bf730838c6ee5a9",
+    "url": "/static/js/main.14680311.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b3edaa289bbf8d4a2992",
+    "url": "/static/js/2.ac728db7.chunk.js"
+  },
+  {
+    "revision": "9426d59aa7ab270cecc19f17db99984c",
+    "url": "/static/media/newLogo.9426d59a.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  },
+  {
+    "revision": "31f3608048d02b97c8af5653e73113ec",
+    "url": "/index.html"
+  }
+];
